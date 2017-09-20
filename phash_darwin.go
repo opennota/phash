dfa8fb714e2fd324895c9f605bdd534ec3074d09
opennota <opennota@gmail.com>
2017-09-20 19:30:53 +0700

    Add build flags for MacOS

---
+++ phash_darwin.go
@@ -0,0 +1,5 @@
+package phash
+
+// #cgo CXXFLAGS: -I/usr/local/opt/libjpeg/include -I/usr/local/opt/libpng/include -I/usr/local/opt/libtiff/include
+// #cgo LDFLAGS: -L/usr/local/opt/libjpeg/lib -L/usr/local/opt/libpng/lib -L/usr/local/opt/libtiff/lib
+import "C"
